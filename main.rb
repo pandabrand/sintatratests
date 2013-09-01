191853cd74f935bc46b5e61c70d03ee7c7439d58
Frederick Wells <pandabrand@gmail.com>
2013-09-01 16:56:16 -0500

    changed to a modular style instead fo classic style app

--- main.rb
+++ main.rb
@@ -6,108 +6,109 @@ require 'sass'
 require 'pony'
 require 'coffee-script'
 
-require './song'
-require './sinatra/auth'
+require_relative 'song'
+require_relative 'sinatra/auth'
+require_relative 'asset-handler'
 
 require 'sinatra/reloader' if development?
 require 'v8' if development?
 
-configure do
-  enable :sessions
-  set :username, 'frank'
-  set :password, 'sinatra'
-end
+class Website < Sinatra::Base
+	use AssetHandler
+	register Sinatra::Auth
+	register Sinatra::Flash
+	
+	set :public_folder, 'assets'
+	set :views, 'templates'
+	
+	configure do
+		enable :sessions
+		set :username, 'frank'
+		set :password, 'sinatra'
+	end
 
-configure :development do
-  DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
-  set :email_address    => 'smtp.gmail.com',
-  		:email_user_name	=> 'pandabrand',
-  		:email_password		=> 'secret',
-  		:email_domain			=> 'localhost.localdomain'
-end
+	configure :development do
+		#DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+		set :email_address    => 'smtp.gmail.com',
+				:email_user_name	=> 'pandabrand',
+				:email_password		=> 'secret',
+				:email_domain			=> 'localhost.localdomain'
+	end
 
-configure :production do
-	DataMapper.setup(:default, ENV['DATABASE_URL'])
-  set :email_address    => 'smtp.sendgrid.net',
-  		:email_user_name	=> ENV['SENDGRID_USERNAME'],
-  		:email_password		=> ENV['SENDGRID_PASSWORD'],
-  		:email_domain			=> 'heroku.com'
-end
+	configure :production do
+		#DataMapper.setup(:default, ENV['DATABASE_URL'])
+		set :email_address    => 'smtp.sendgrid.net',
+				:email_user_name	=> ENV['SENDGRID_USERNAME'],
+				:email_password		=> ENV['SENDGRID_PASSWORD'],
+				:email_domain			=> 'heroku.com'
+	end
 
-set :public_folder, 'assets'
-set :views, 'templates'
-set :session_secret, 'this is a long boring secret to the make it hard to figure out'
+	before do
+		set_title
+	end
+	
+	def css(*stylesheets)
+		stylesheets.map do |stylesheet|
+			"<link href=\"/#{stylesheet}.css\" media=\"screen, production\" rel=\"stylesheet\" />"
+		end.join
+	end
 
-before do
-	set_title
-end
+	def current?(path='/')
+		(request.path==path || request.path==path+'/') ? "current" : nil
+	end
 
-get('/styles.css'){ scss :styles }
-get('/js/application.js'){ coffee :application }
+	def set_title
+		@title ||= "Songs By Sinatra"
+	end
 
-get '/' do
-  erb :home
-end
+	def send_message
+		Pony.mail(
+			:from => params[:name] + "<" + params[:email] + ">",
+			:to => 'pandabrand@gmail.com',
+			:subject => params[:name] + " has contacted you",
+			:body => params[:message],
+			:port => '587',
+			:via => :smtp,
+			:via_options => {
+				:address	=> 'smtp.gmail.com',
+				:port			=> '587',
+				:enable_srttls_auto	=> 'true',
+				:user_name	=> 'pandabrand',
+				:password	=> 'secret',
+				:authentication => :plain,
+				:domain 	=> 'localhost.localdomain'
+		})
+	end
 
-get '/about' do
-  @title = "All About This Website"
-  erb :about
-end
+	get '/' do
+		erb :home
+	end
 
-get '/contact' do
- erb :contact
-end
+	get '/about' do
+		@title = "All About This Website"
+		erb :about
+	end
 
-get '/set/:name' do
-  session[:name] = params[:name]
-end
+	get '/contact' do
+	 erb :contact
+	end
 
-get '/get/hello' do
-  "Hello #{session[:name]}"
-end
+	get '/set/:name' do
+		session[:name] = params[:name]
+	end
 
-post '/contact' do
-	send_message
-	flash[:notice] = "Thank you for your message. We'll be in touch soon."
-	redirect to('/')
-end
+	get '/get/hello' do
+		"Hello #{session[:name]}"
+	end
 
-not_found do
-	puts request.path_info
-  erb :not_found
-end
+	post '/contact' do
+		send_message
+		flash[:notice] = "Thank you for your message. We'll be in touch soon."
+		redirect to('/')
+	end
 
-helpers do
-  def css(*stylesheets)
-  	stylesheets.map do |stylesheet|
-  		"<link href=\"/#{stylesheet}.css\" media=\"screen, production\" rel=\"stylesheet\" />"
-  	end.join
-  end
-  
-  def current?(path='/')
-  	(request.path==path || request.path==path+'/') ? "current" : nil
-  end
-  
-  def set_title
-  	@title ||= "Songs By Sinatra"
-  end
-  
-  def send_message
-  	Pony.mail(
-  		:from => params[:name] + "<" + params[:email] + ">",
-  		:to => 'pandabrand@gmail.com',
-  		:subject => params[:name] + " has contacted you",
-  		:body => params[:message],
-  		:port => '587',
-  		:via => :smtp,
-  		:via_options => {
-  			:address	=> 'smtp.gmail.com',
-  			:port			=> '587',
-  			:enable_srttls_auto	=> 'true',
-  			:user_name	=> 'pandabrand',
-  			:password	=> 'secret',
-  			:authentication => :plain,
-  			:domain 	=> 'localhost.localdomain'
-  	})
-  end
+	not_found do
+		puts request.path_info
+		erb :not_found
+	end
 end
--- song.rb
+++ song.rb
@@ -1,5 +1,11 @@
+require 'sinatra/base'
 require 'dm-core'
 require 'dm-migrations'
+require 'sass'
+require 'sinatra/flash'
+require_relative 'sinatra/auth'
+require_relative 'asset-handler'
+
 
 class Song
   include DataMapper::Resource
@@ -28,59 +34,102 @@ module SongHelpers
 		@song = Song.create(params[:song])
 	end
 end
-helpers SongHelpers
+
+class SongController < Sinatra::Base
+	use AssetHandler
+	enable :method_override
+	register Sinatra::Flash
+	register Sinatra::Auth
+	helpers SongHelpers
+	
+	set :public_folder, 'assets'
+	set :views, 'templates'
+	
+	configure do
+		enable :sessions
+		set :username, 'frank'
+		set :password, 'sinatra'
+	end
+
+	configure :development do
+		DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+	end
+
+	configure :production do
+		DataMapper.setup(:default, ENV['DATABASE_URL'])
+	end
+
+	before do
+		set_title
+	end
+	
+	def css(*stylesheets)
+		stylesheets.map do |stylesheet|
+			"<link href=\"/#{stylesheet}.css\" media=\"screen, production\" rel=\"stylesheet\" />"
+		end.join
+	end
+
+	def current?(path='/')
+		(request.path==path || request.path==path+'/') ? "current" : nil
+	end
+
+	def set_title
+		@title ||= "Songs By Sinatra"
+	end
+
+	get '/' do
+		find_songs
+		erb :songs
+	end
+
+	get '/new' do
+		protected!
+		@song = Song.new
+		erb :new_song
+	end
+
+	get '/:id' do
+		@song = find_song
+		erb :show_song
+	end
+
+	get '/:id/edit' do
+		protected!
+		@song = find_song
+		erb :edit_song
+	end
+
+	put '/:id' do
+		protected!
+		song = find_song
+		if song.update(params[:song])
+			flash[:notice] = "Song successfully updated" 
+		end
+		redirect to("/#{song.id}")
+	end
+
+	post '/' do
+		protected!
+		flash[:notice] = "Song successfully added" if create_song
+		redirect to("/#{@song.id}")
+	end
+
+	delete '/:id' do
+		protected!
+		if find_song.destroy
+			flash[:notice] = "Song deleted"
+		end
+		redirect to('/')
+	end
+
+	post '/:id/like' do
+		@song = find_song
+		@song.likes = @song.likes.next
+		@song.save
+		redirect to"/#{@song.id}" unless request.xhr?
+		erb :likes, :layout => false
+	end
+end
 
 DataMapper.finalize
 
-get '/songs' do
-  find_songs
-  erb :songs
-end
-
-get '/songs/new' do
-	protected!
-  @song = Song.new
-  erb :new_song
-end
-
-get '/songs/:id' do
-  @song = find_song
-  erb :show_song
-end
-
-get '/songs/:id/edit' do
-	protected!
-  @song = find_song
-  erb :edit_song
-end
-
-put '/songs/:id' do
-	protected!
-  song = find_song
-  if song.update(params[:song])
-  	flash[:notice] = "Song successfully updated" 
-  end
-  redirect to("/songs/#{song.id}")
-end
-
-post '/songs' do
-	protected!
- 	flash[:notice] = "Song successfully added" if create_song
-  redirect to("songs/#{@song.id}")
-end
-
-delete '/songs/:id' do
-	protected!
-  if find_song.destroy
-  	flash[:notice] = "Song deleted"
-  end
-  redirect to('/songs')
-end
-
-post '/songs/:id/like' do
-	@song = find_song
-	@song.likes = @song.likes.next
-	@song.save
-	redirect to"/songs/#{@song.id}" unless request.xhr?
-	erb :likes, :layout => false
-end
